bfd4aa903f5db5863f2ec2ebb63db9984427c010
Evan <Ragone>
2017-09-30 17:10:43 -0400

    test

---
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/LAutonomusBlue.java
@@ -0,0 +1,92 @@
+package org.firstinspires.ftc.robotcontroller.internal;
+
+import android.app.DownloadManager;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.I2cAddr;
+@Autonomous(name="autoBlue")
+public class LAutonomusBlue extends LinearOpMode {
+    DcMotor frontLeft;
+    DcMotor frontRight;
+    DcMotor backLeft;
+    DcMotor backRight;
+    DcMotor Raise;
+    DcMotor Pull;
+    DcMotor Knocker;
+    ColorSensor Color;
+    public void forward(double power) {
+        frontLeft.setPower(power);
+        frontRight.setPower(power);
+        backLeft.setPower(power);
+        backRight.setPower(power);
+    }
+    public void right(double power) {
+        frontLeft.setPower(power);
+        frontRight.setPower(-power);
+        backLeft.setPower(-power);
+        backRight.setPower(power);
+    }
+    public void left(double power) {
+        frontLeft.setPower(-power);
+        frontRight.setPower(power);
+        backLeft.setPower(power);
+        backRight.setPower(-power);
+    }
+    public void backwards(double power) {
+        frontLeft.setPower(-power);
+        frontRight.setPower(-power);
+        backLeft.setPower(-power);
+        backRight.setPower(-power);
+    }
+
+    @Override
+    public void runOpMode() {
+        frontLeft = hardwareMap.dcMotor.get("FrontLeft");
+        frontRight = hardwareMap.dcMotor.get("FrontRight");
+        backLeft = hardwareMap.dcMotor.get("BackLeft");
+        backRight = hardwareMap.dcMotor.get("BackRight");
+        Raise = hardwareMap.dcMotor.get("Raise");
+        Pull = hardwareMap.dcMotor.get("Pull");
+        Knocker = hardwareMap.dcMotor.get("Knock");
+
+        Color = hardwareMap.colorSensor.get("Color");
+        Color.setI2cAddress(I2cAddr.create7bit(0x1f));
+        frontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+        backLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+        frontRight.setDirection(DcMotorSimple.Direction.FORWARD);
+        backRight.setDirection(DcMotorSimple.Direction.FORWARD);
+        Raise.setDirection(DcMotorSimple.Direction.FORWARD);
+        Pull.setDirection(DcMotorSimple.Direction.REVERSE);
+        Knocker.setDirection(DcMotorSimple.Direction.FORWARD);
+        frontLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        backLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        frontRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        backRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        Raise.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        Pull.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        Knocker.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        frontLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        frontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        Raise.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        Pull.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        Knocker.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        Color.enableLed(false);
+        while (!isStarted()) ;
+        forward(1);
+        try {
+            Thread.sleep(150);
+        } catch (InterruptedException e) {
+            telemetry.addData("Error", "Failed to Sleep");
+        }
+
+        requestOpModeStop();
+    }
+}
+
+
--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/Teleop.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/Teleop.java
@@ -15,10 +15,13 @@ public class Teleop extends OpMode {
     private DcMotor FrontRight;
     private DcMotor BackLeft;
     private DcMotor BackRight;
-    private DcMotor LaunchL;
-    private DcMotor LaunchR;
-    private DcMotor load;
-    private Servo block;
+    private DcMotor Knocker;
+    private DcMotor Raise;
+    private DcMotor Pull;
+    private Servo Release;
+    private Servo Rclose;
+    private Servo Lclose;
+    private Servo Relic;
     @Override
     public void init() {
         //dcMotors setting
@@ -26,10 +29,13 @@ public class Teleop extends OpMode {
         FrontRight = hardwareMap.dcMotor.get("FrontRight");
         BackLeft = hardwareMap.dcMotor.get("BackLeft");
         BackRight = hardwareMap.dcMotor.get("BackRight");
-        LaunchL = hardwareMap.dcMotor.get("LaunchL");
-        LaunchR = hardwareMap.dcMotor.get("LaunchR");
-        load = hardwareMap.dcMotor.get("load");
-        block = hardwareMap.servo.get("block");
+        Raise = hardwareMap.dcMotor.get("Raise");
+        Pull = hardwareMap.dcMotor.get("Pull");
+        Knocker = hardwareMap.dcMotor.get("Knock");
+        Release = hardwareMap.servo.get("Release");
+        Rclose = hardwareMap.servo.get("Rclose");
+        Lclose = hardwareMap.servo.get("Lclose");
+        Relic = hardwareMap.servo.get("Relic");
 
 
         //If wrong change to REVERSE
@@ -38,24 +44,25 @@ public class Teleop extends OpMode {
         BackLeft.setDirection(DcMotorSimple.Direction.FORWARD);
         FrontRight.setDirection(DcMotorSimple.Direction.REVERSE);
         BackRight.setDirection(DcMotorSimple.Direction.REVERSE);
-        LaunchL.setDirection(DcMotorSimple.Direction.FORWARD);
-        LaunchR.setDirection(DcMotorSimple.Direction.REVERSE);
-        load.setDirection(DcMotorSimple.Direction.FORWARD);
+        Raise.setDirection(DcMotorSimple.Direction.FORWARD);
+        Pull.setDirection(DcMotorSimple.Direction.REVERSE);
+        Knocker.setDirection(DcMotorSimple.Direction.FORWARD);
+
         FrontLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         BackLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         FrontRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         BackRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        LaunchR.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        LaunchL.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        load.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        Raise.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        Pull.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        Knocker.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
         FrontLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         FrontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         BackLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         BackRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        LaunchL.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        LaunchR.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        load.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        Raise.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        Pull.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        Knocker.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
         telemetry.addData("Init", "Init Completed");
     }
@@ -80,27 +87,29 @@ public class Teleop extends OpMode {
             BackLeft.setPower(gamepad1.left_stick_x + gamepad1.left_stick_y);
             FrontRight.setPower(gamepad1.left_stick_x + gamepad1.left_stick_y);
         }
-        if (gamepad1.right_bumper) {
-            LaunchL.setPower(1);
-            LaunchR.setPower(1);
-        }
-        if(gamepad1.right_bumper) {
-            LaunchL.setPower(0);
-            LaunchR.setPower(0);
-        }
-        if(gamepad1.left_bumper) {
-            load.setPower(1);
-        }
-        if(gamepad1.left_bumper) {
-            load.setPower(0);
-        }
-        if(gamepad1.a) {
-            block.setPosition(1);
-        }
-        if (gamepad1.b) {
-            block.setPosition(0.5);
-        }
-
+      if(gamepad1.left_bumper) {
+          Release.setPosition(0.5);
+      }
+      if (gamepad1.right_bumper)
+          Pull.setPower(1);
+      if(gamepad1.dpad_up){
+          Raise.setPower(1);
+          //attach facing same orientation as wheels
+      }
+      if (gamepad1.dpad_down) {
+          Raise.setPower(-1);
+      }
+      if (gamepad1.a) {
+          Lclose.setPosition(0.6);
+          Rclose.setPosition(0.6);
+      }
+      if (gamepad1.b) {
+          Lclose.setPosition(0);
+          Rclose.setPosition(0);
+      }
+      if(gamepad1.y) {
+          Relic.setPosition(1);
+      }
         telemetry.addData("Loop", "Running" );
     }
 
